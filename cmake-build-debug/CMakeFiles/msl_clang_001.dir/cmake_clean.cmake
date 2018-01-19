299798608c5add8d543dc6fd0c39359fe2c39d4f
Matthew Moltzau <matthew@moltzau.net>
2018-01-18 21:43:18 -0700

    First Update
    I just opened it in CLion and it made some files. I figured I might as
    well test the git in the meantime and that we would probably continue
    to use the generated files.

---
+++ cmake-build-debug/CMakeFiles/msl_clang_001.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msl_clang_001.dir/main.c.o"
+  "msl_clang_001.pdb"
+  "msl_clang_001"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/msl_clang_001.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
